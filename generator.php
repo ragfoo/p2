85103061c75a527d31e4115a1e67f66f58d81aa3
Ryan Gallagher <ryan_gallagher@apple.com>
2016-09-22 16:57:50 -0400

    styles, fix input, fix logic

--- generator.php
+++ generator.php
@@ -3,24 +3,61 @@
 
 $csv = array_map('str_getcsv', file('shakespeare.csv'));
 $numberOfWords = count($csv);
+$error = "";
+$defaultWordCount = 4;
+$addNumber = 'off';
+$addSymbol = 'off';
+$symbolArray = ['!','@','#','$','%','^','&','*','?','+'];
+$wordCount = $defaultWordCount;
 
-//$testVariable = $_POST['word_count'];
+if (array_key_exists('word_count',$_POST)){
+	global $wordCount;
+	$wordCount = $_POST['word_count'];
+}
 
-$password = "";
+if (array_key_exists('add_number',$_POST)){
+	$addNumber = $_POST['add_number'];
+}
 
-function generatePassword($wordCount){
-	global $csv;
-	global $numberOfWords;
-	$wordList = [
-	    'Apples',
-	    'Oranges',
-	    'Milk'
-	    ];
-	
-	$password = $csv[rand(0,$numberOfWords-1)][0];
-	for($i = 1; $i < $wordCount; $i++){
-		$password = $password."-".$csv[rand(0,$numberOfWords-1)][0];
+if (array_key_exists('add_symbol',$_POST)){
+	$addSymbol = $_POST['add_symbol'];
+}
+
+$password = generatePassword($wordCount,$addNumber,$addSymbol);
+
+
+
+function generatePassword($wordCount,$addNumber,$addSymbol){
+	global $error;
+	$error = "";
+	if ((intval($wordCount) >= 0) and (intval($wordCount) < 10)){
+		global $csv;
+		global $numberOfWords;
+		
+		if (intval($wordCount) == 0){
+			$password = "";
+		}
+		else{
+			$password = $csv[rand(0,$numberOfWords-1)][0];
+		}
+		
+		for($i = 1; $i < $wordCount; $i++){
+			$password = $password."-".$csv[rand(0,$numberOfWords-1)][0];
+		}
+		
+		if ($addNumber == 'on'){
+			$password = $password.rand(0,9999);
+		}
+		
+		if ($addSymbol == 'on'){
+			global $symbolArray;
+			$numberOfSymbols = count($symbolArray);
+			$password = $password.$symbolArray[rand(0,$numberOfSymbols-1)];
+		}
+		return $password;
 	}
-	
-	return $password;
+	else{
+		$error = "The number of words must be an integer between 1 and 9";
+		return 1;
+	}
 }
--- index.php
+++ index.php
@@ -4,23 +4,66 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 ?>
 
 <!DOCTYPE html>
-<html>
-<head>
-
-    <meta charset='utf-8'>
-    <title>Password Generator</title>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Shakespearian Password Generator</title>
 	<?php require 'generator.php'; ?>
-</head>
+	
+	<link href='css/styles.css' rel='stylesheet'>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 <body>
-	<form method='POST' action='index.php'>
-	    Number of Words<br>
-	    <input type='text' name='word_count'><br>
-	    <input type='submit' value='Another'><br>
-	</form>
-		<?php
+	<div class='container'>
+		<h1>Shakespearian Password Generator</h1>
+
+		<p class='password'>
+			<?php
+				echo $password
+			?>
+		</p>
+
+		<form method='POST' action='index.php'>
+			<p class='options'>
+
+				<label for='number_of_words'>Number of Words</label>
+				<input maxlength=1 type='text' name='word_count' id='word_count' value=<?php echo "\"".$wordCount."\"" ?> >  (Max 9)
+				<br>
+
+				<input type='checkbox' name='add_number' id='add_number' <?php if ($addNumber == 'on'){echo 'checked';}?>>
+				<label for='add_number'>Add a number</label>
+				<br>
+				<input type='checkbox' name='add_symbol' id='add_symbol' <?php if ($addSymbol == 'on'){echo 'checked';}?>>
+				<label for='add_symbol'>Add a symbol</label>
+			</p>
+
+			<input type='submit' class='btn btn-default' value='Another'>
+
+							<div class='error'>
+								<?php echo $error?>
+									</div>
+			
+		</form>
+	
+
+	
+	<img class="img-responsive" src="/img/shakespeare.jpg" alt="shakespeare" width="2000" height="345">
 		
-			echo generatePassword($_POST['word_count']);
-		?>
+	</div>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
